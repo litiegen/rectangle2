111c24f1180ee95716005eff1617a74fddae7d0c
litiegen <517206296@qq.com>
2020-03-13 14:12:29 +0800

    a

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,8 +17,69 @@ module.exports = function (grunt) {
         htmlhintrc: '.htmlhintrc'
       },
       src: ['*.html']
+    },
+
+    mochacli: {
+      options: {
+        reporter: 'spec',
+        bail: true
+      },
+      all: ['test/*.js']
+    },
+    mocha_istanbul: {
+      coverage: {
+        src: 'test'
+      }
+    },
+    istanbul_check_coverage: {
+      default: {
+        options: {
+          coverageFolder: 'coverage*',
+          check: {
+            lines: 90,
+            statements: 90
+          }
+        }
+      }
+    },
+    mocha: {
+      test: {
+        src: ['test/index.html'],
+      },
+      options: {
+        run: true,
+        reporter: 'Dot'
+      }
+    },
+    run: {
+      api: {
+        options: { wait: false },
+        args: ['./app.js']
+      }
     }
+
+
+
+
   });
+
+
+  grunt.loadNpmTasks('grunt-run')
+  grunt.loadNpmTasks('grunt-mocha-cli')
+
+  grunt.registerTask('default', ['run', 'mochacli', 'stop:api']);
+
+  grunt.loadNpmTasks('grunt-mocha');
+
+  grunt.registerTask('default', ['mocha']);
+
+  grunt.loadNpmTasks('grunt-mocha-cli');
+  grunt.loadNpmTasks('grunt-mocha-istanbul');
+
+  grunt.registerTask('default', ['mochacli']);
+  grunt.registerTask('cover', ['mocha_istanbul']);
+  grunt.registerTask('cover-check', ['istanbul_check_coverage']);
+
 
   grunt.loadNpmTasks('grunt-htmlhint');
   grunt.loadNpmTasks('grunt-eslint');
