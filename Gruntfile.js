7203678b71d9dc6c793309228812f2cfdfc5f2b0
litiegen <517206296@qq.com>
2020-04-06 12:29:33 +0800

    a

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,46 +1,23 @@
+/* global module: true */
 module.exports = function (grunt) {
   grunt.initConfig({
-    csslint: {
-      options: {
-        csslintrc: '.csslintrc'
-      },
-      src: ['*.css']
-    },
     eslint: {
       options: {
         configFile: '.eslintrc.json'
       },
       target: ['*.js']
     },
+    csslint: {
+      options: {
+        csslintrc: '.csslintrc'
+      },
+      src: '*.css'
+    },
     htmlhint: {
       options: {
         htmlhintrc: '.htmlhintrc'
       },
-      src: ['*.html']
-    },
-
-    mochacli: {
-      options: {
-        reporter: 'spec',
-        bail: true
-      },
-      all: ['test/*.js']
-    },
-    mocha_istanbul: {
-      coverage: {
-        src: 'test'
-      }
-    },
-    istanbul_check_coverage: {
-      default: {
-        options: {
-          coverageFolder: 'coverage*',
-          check: {
-            lines: 90,
-            statements: 90
-          }
-        }
-      }
+      src: '*.html'
     },
     mocha: {
       test: {
@@ -48,44 +25,69 @@ module.exports = function (grunt) {
       },
       options: {
         run: true,
-        reporter: 'Dot'
+        reporter: 'Spec'
       }
     },
-    run: {
-      api: {
-        options: { wait: false },
-        args: ['./app.js']
+    htmlmin: {
+      options: {
+        collapseWhitespace: true,
+        preserveLineBreaks: false
+      },
+      files: {
+        src: 'dist/index.html',
+        dest: 'dist/index.html'
+      }
+    },
+    cssmin: {
+      'dist/rectangle.css': 'rectangle.css'
+    },
+    uglify: {
+      release:{
+        files: {
+          'dist/bundle.min.js': 'dist/bundle.js',
+        }
+      }
+    },
+    useminPrepare: {
+      html: 'index.html',
+      options: {
+        dest: 'dist'
+      }
+    },
+    usemin: {
+      html: ['dist/index.html']
+    },
+    concat: {
+      options: {
+        separator: ';'
+      },
+      js: {
+        src: ['rectangle.js', 'calc.js'],
+        dest: 'dist/bundle.js'
+      }
+    },
+    clean: ['dist/bundle.js', '.tmp'],
+    copy: {
+      html: {
+        src: './index.html',
+        dest: './dist/index.html'
       }
     }
-
-
-
-
   });
 
-
-  grunt.loadNpmTasks('grunt-run')
-  grunt.loadNpmTasks('grunt-mocha-cli')
-
-  grunt.registerTask('default', ['run', 'mochacli', 'stop:api']);
-
-  grunt.loadNpmTasks('grunt-mocha');
-
-  grunt.registerTask('default', ['mocha']);
-
-  grunt.loadNpmTasks('grunt-mocha-cli');
-  grunt.loadNpmTasks('grunt-mocha-istanbul');
-
-  grunt.registerTask('default', ['mochacli']);
-  grunt.registerTask('cover', ['mocha_istanbul']);
-  grunt.registerTask('cover-check', ['istanbul_check_coverage']);
-
-
+  grunt.loadNpmTasks('grunt-contrib-csslint');
   grunt.loadNpmTasks('grunt-htmlhint');
   grunt.loadNpmTasks('grunt-eslint');
-  grunt.loadNpmTasks('grunt-contrib-csslint');
+  grunt.loadNpmTasks('grunt-mocha');
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-usemin');
+  grunt.loadNpmTasks('grunt-contrib-clean');
 
-  grunt.registerTask('default', ['htmlhint']);
-  grunt.registerTask('default', ['csslint']);
-  grunt.registerTask('default', ['eslint']);
+  grunt.registerTask('lint', ['htmlhint', 'csslint', 'eslint']);
+  grunt.registerTask('unitTest', ['mocha']);
+  grunt.registerTask('release', ['copy', 'useminPrepare', 'concat', 'uglify', 'usemin', 'cssmin', 'htmlmin', 'clean']);
 };
